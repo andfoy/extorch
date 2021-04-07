74d9b31c2e349b4652ba75838c073d0c8a5160a3
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-04-06 20:15:45 -0500

    Include plus sign

--- .github/scripts/download_torch.sh
+++ .github/scripts/download_torch.sh
@@ -2,20 +2,22 @@ set -exou
 
 BASE_URL="https://download.pytorch.org/libtorch"
 
+FILENAME="libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}.zip"
+ZIP_FILENAME=$FILENAME
+
 if [[ $PYTORCH_VERSION == "latest" ]]; then
     BASE_URL="${BASE_URL}/nightly"
-fi
-
-FILENAME="libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}.zip"
-
-if [[ $DEVICE == "cpu" ]]; then
-    FILENAME="libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+else
+    if [[ $DEVICE == "cpu" ]]; then
+        FILENAME="libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+        ZIP_FILENAME="libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}+cpu.zip"
+    fi
 fi
 
 BASE_URL="${BASE_URL}/${DEVICE}/${FILENAME}"
 
 pushd native/extorch_native
-wget $BASE_URL
-unzip $FILENAME
-rm -rf $FILENAME
+wget $BASE_URL -q --show-progress
+unzip $ZIP_FILENAME
+rm -rf $ZIP_FILENAME
 popd
