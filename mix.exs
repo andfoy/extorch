dbe137531b1645fef9e7a3b057f8ad9cda13d97f
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-08-15 18:42:21 -0500

    Apply formatter

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule ExTorch.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ssl, :inets],
+      extra_applications: [:logger, :ssl, :inets]
     ]
   end
 
